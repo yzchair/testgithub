0027830a52640c093a15822a6e4f4f3adcff0b6b
13967 <1396748139@qq.com>
2019-04-11 08:55:20 +0800

    Data Type Conversion Modify1

--- com/common/SwapStrSb.java
+++ com/common/SwapStrSb.java
@@ -6,9 +6,9 @@ public class SwapStrSb {
         String str = new String("This is a string");
 
         // 构造方法
-        StringBuilder sb1 = new StringBuilder(str);
+        StringBuffer sb1 = new StringBuffer(str);
         // append()方法
-        StringBuilder sb2 = new StringBuilder();
+        StringBuffer sb2 = new StringBuffer();
         sb2.append(str);
 
         System.out.println("String类型转换为StringBuffer类型 : ");
@@ -18,7 +18,7 @@ public class SwapStrSb {
         System.out.println();
 
         // StringBuffer转换为String
-        StringBuilder sb = new StringBuilder("This is a StringBuffer");
+        StringBuffer sb = new StringBuffer("This is a StringBuffer");
 
         // 构造方法
         String str1 = new String(sb);
@@ -28,6 +28,27 @@ public class SwapStrSb {
         System.out.println("StringBuffer类型转换为String类型 : ");
         System.out.println("构造方法 : " + str1);
         System.out.println("toString()方法 : " + str2);
+
+        System.out.println();
+
+        StringBuffer sb3 = new StringBuffer("abc def 123");
+
+        // 插入操作
+        StringBuffer sb4 = new StringBuffer("append ");
+        sb4.append(sb3);
+        System.out.println("添加 : " + sb4);
+
+        // 插入操作
+        sb3.insert(4, "insert");
+        System.out.println("插入 : " + sb3);
+
+        // 删除操作
+        sb3.delete(2, 5);
+        System.out.println("删除 : " + sb3);
+
+        // 反转操作
+        sb3.reverse();
+        System.out.println("反转 : " + sb3);
 
     }
 }
